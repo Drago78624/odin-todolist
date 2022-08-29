b926d51f04e218e2ed5623d948843e86f6820301
Drago78624 <maazahmed78624@gmail.com>
2022-08-29 08:58:23 +0500

    projects modal added

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import './style.css'
+
+const projectsModal = document.getElementById("projects-modal");
+const projectsModalBtn = document.getElementById("openProjectsModal");
+
+projectsModalBtn.addEventListener("click", () => {
+  projectsModal.style.display = "flex";
+});
+
+window.onclick = function (event) {
+  if (event.target == projectsModal) {
+    projectsModal.style.display = "none";
+  }
+};
